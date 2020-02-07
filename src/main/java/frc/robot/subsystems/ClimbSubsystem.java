a722cdacbd931597d80e3574840b5b309c0e8e02
ReillyO <57842556+ReillyO@users.noreply.github.com>
2020-02-07 07:02:16 -0500

    Update ClimbSubsystem.java

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -3,40 +3,74 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import frc.robot.Constants;
 import edu.wpi.first.wpilibj.Encoder;
 import frc.robot.OI;
 
 public class ClimbSubsystem extends Subsystem {
     OI oi = new OI();
-    private WPI_TalonSRX climbController = new WPI_TalonSRX(6);
-    private double speed = 0.32;
+    private WPI_TalonSRX climbController = new WPI_TalonSRX(Constants.ClimbMotorPortNumber);
+  
+    Encoder liftMotorEncoder ;
 
-    public ClimbSubsystem(){}
+    public ClimbSubsystem(){
+       liftMotorEncoder = new Encoder(Constants.LiftEncoderChannelA, Constants.LiftEncoderChannelB);
+
+    }
 
     @Override
 	protected void initDefaultCommand() {
     }
     
-    public void moveUp(){
+    enum ClimbState { 
+        Idle,
+        LiftHook,
+        HookAtTop,
+        LiftingBot,
+        Stopped	
+    } 
+
+    ClimbState climbState ;
+
+    void OperateClimb() {
+        switch (climbState){
+            case Idle:
+                if(oi.getSquareButton()){
+                    moveMotor(Constants.LiftMotorSpeed);
+                    liftMotorEncoder.reset();
+                    climbState = ClimbState.LiftHook ; 
+                }
+                break ;
+            case LiftHook:
+                if(liftMotorEncoder.get() >= Constants.MaxEncoderValue){
+                    stopMotor();
+                    climbState = ClimbState.HookAtTop ;
+                }
+                break ;
+            case HookAtTop:
+                if(oi.getXButton()){
+                    moveMotor(-Constants.LiftMotorSpeed);
+                    liftMotorEncoder.reset();
+                    climbState = ClimbState.LiftingBot ;
+                }
+                break ;
+            case LiftingBot:
+                if(liftMotorEncoder.get() <= 0){
+                    stopMotor();
+                    climbState = ClimbState.Stopped ;
+                }
+                break ;
+            case Stopped:
+                //Bot is immobile
+                break ;
+        }
+    }
+
+    public void moveMotor(double speed){
         climbController.set(speed);
     }
-    public void moveDown(){
-        speed = -speed;
-        climbController.set(speed);
+
+    public void stopMotor(){
+        climbController.set(0);
     }
-    
-    public void stopClimb(){
-        speed = 0;
-      }
-
-    // public void OperateLift() {
-    //     switch (e){
-
-            
-    //     }
-
-
-
-    // }
-
 }
