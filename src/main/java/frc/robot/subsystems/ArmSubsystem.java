da6100b57b854e5fca467f4a0ec2960ec2389716
ReillyO <57842556+ReillyO@users.noreply.github.com>
2020-02-12 17:30:36 -0500

    Update ArmSubsystem.java

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,157 +4,223 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+ 
 package frc.robot.subsystems;
-
+//servo port 0 
+ 
+//sure we'll say the motor is port 1
+ 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Spark;
+import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.Solenoid;
 import frc.robot.Constants;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.SendableBase;
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.OI;
-
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.I2C;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
+import frc.robot.components.ColourFilter;
+ 
 /**
  * Add your docs here.
  */
+ 
 public class ArmSubsystem extends Subsystem {
+  char colourGoal;
+ 
   OI oi = new OI();
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-
-
-  int colourChangeCounter = 0 ;
-
-  String currentColour= " ";
-  String lastColour= " ";
-
-  private Servo armServo = new Servo(0);
-  private Spark spinMotor = new Spark(1);
-
-  long waitStartTime ;
  
-  enum ArmState { Idle, DeployingArm, SpinningWheel, WaitingForTimeout, RetractingArm } ;
-  ArmState armState ;
-   
+  int colourChangeCounter = 0;
+ 
+  char currentColour = 'a';
+  char lastColour = 'b';
+ 
+  private final static I2C.Port i2cPort = I2C.Port.kOnboard;
+  final static ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+ 
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+ 
+ 
+  //put calibrated colours here
+ 
+  private Solenoid armPiston = new Solenoid(0);
+  private Spark spinMotor = new Spark(1);
+ 
+  ColourFilter colourFilter;
+ 
+  long waitStartTime;
+ 
+  enum ArmState {
+    Idle, DeployingArm, SpinningWheel, WaitingForTimeout, RetractingArm
+  };
+ 
+  ArmState armState;
+ 
+  enum SpinningMode{
+    Which, TurnTimes, TurnColour
+  };
+ 
+  SpinningMode spinningMode;
+ 
+ 
   public ArmSubsystem() {
-      armState = ArmState.Idle ;
+    armState = ArmState.Idle;
+    colourFilter = new ColourFilter(Constants.colourFilterLength, 'n');
   }
  
   @Override
   public void initDefaultCommand() {
-    armServo.set(0.0) ;
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+    spinMotor.set(0.0);
+    armPiston.set(false);
   }
  
-  
   public void OperateControlPanel() {
+    System.out.println("entered arm");
     switch (armState) {
-      case Idle:
-          spinMotor.set(0.0) ;
-         armServo.set(0.0) ;
-         if (oi.getSquareButton()) {
-           armServo.setAngle(Constants.deployAngle);
-           armState = ArmState.DeployingArm ;
-         }
-         break ;
-      case DeployingArm:
-          if (!oi.getSquareButton()){
-            armState = ArmState.Idle;
-          } 
-          else if (armServo.getAngle() >= 89) {
-            armState = ArmState.SpinningWheel;
-            colourChangeCounter = 0 ;
-            lastColour = getColour() ;
-            spinMotor.set(Constants.maxSpinSpeed ) ;
-          }
-          break ;
-      case SpinningWheel:
-        if (!oi.getSquareButton()){
-          armState = ArmState.Idle;
-        } 
-         else{
-            currentColour = getColour() ;
-            if(currentColour!=lastColour){
-             colourChangeCounter++;
-            }
-            if(colourChangeCounter > Constants.minColorChangeCountGoal ){
-              waitStartTime = System.currentTimeMillis();
-              armState = ArmState.WaitingForTimeout ;
-              spinMotor.set(0.0) ;
+    case Idle:
+      spinMotor.set(0.0);
+      armPiston.set(false);
+      if (oi.getSquareButton()) { //what button is used ?? MUST CHANGE
+        armPiston.set(true);
+        armState = ArmState.DeployingArm;
+      }
+      break;
+    case DeployingArm:
+      if (!oi.getSquareButton()) {
+        armState = ArmState.Idle;
+      } else  {
+        armState = ArmState.SpinningWheel;
+        colourChangeCounter = 0;
+        lastColour = colourFilter.getColour();
+        spinMotor.set(Constants.maxSpinSpeed);
+      }
+      break;
+    case SpinningWheel:
+      if (!oi.getSquareButton()) {
+        armState = ArmState.Idle;
+      } else {
+        switch (spinningMode){
+          case Which:
+            if (getFMSColour == 'n'){
+              spinningMode = SpinningMode.TurnTimes;
             } 
-          }
-         break;
-      case WaitingForTimeout:
-         if (!oi.getSquareButton()){
-            armState = ArmState.Idle;
-          } 
-         else if ( System.currentTimeMillis() - waitStartTime > Constants.wheelWaitTime ) {
-           armServo.set(0.0) ;
-           armState = ArmState.RetractingArm ;
-         }
-         break;
-      case RetractingArm:
-         if ( armServo.getAngle() < 5){
-          armState= ArmState.Idle;
-         }
-         break;
-
-         
+            else{
+              spinningMode = SpinningMode.TurnColour;
+            }
+            break;
+          case TurnTimes:
+            colourFilter.addMeasurement( getColourFromSensor() );
+            currentColour = colourFilter.getColour();
+            if (currentColour != lastColour) {
+              colourChangeCounter++;
+            }
+            if (colourChangeCounter > Constants.minColorChangeCountGoal) {
+              waitStartTime = System.currentTimeMillis();
+              spinMotor.set(0.0);
+              armState = ArmState.WaitingForTimeout;
+            }
+            break;
+          case TurnColour:
+            if (getColourFromSensor == getFMSColour){
+                colourChangeCounter = 0;
+                if (colourChangeCounter != 2){
+                  if (currentColour != lastColour) {
+                    colourChangeCounter++;
+                  }  
+                }
+                else{
+                  spinMotor.set(0.0);
+                  armState = ArmState.WaitingForTimeout;
+                }              
+            }
+            else{
+              while (getColourFromSensor!=getFMSColour){
+                spinMotor.set(Constants.maxSpinSpeed);
+              }
+              colourChangeCounter = 0;
+                if (colourChangeCounter != 2){
+                  if (currentColour != lastColour) {
+                    colourChangeCounter++;
+                  }  
+                }
+                else{
+                  spinMotor.set(0.0);
+                  armState = ArmState.WaitingForTimeout;
+                }              
+            }
+            
+            break;
+        }
+      }
+      break;
+    case WaitingForTimeout:
+      if (!oi.getSquareButton()) {
+        armState = ArmState.Idle;
+      } else if (System.currentTimeMillis() - waitStartTime > Constants.wheelWaitTime) {
+        armPiston.set(false);
+        armState = ArmState.RetractingArm;
+      }
+      break;
+    case RetractingArm:
+      //if (armPiston.get() == false) {
+        armState = ArmState.Idle;
+      //}
+      break;
+ 
     }
-   
+    System.out.println("arm state is " + armState);
+    System.out.println("angle of servo is " + armPiston.get());
+ 
   }
-
-
+ 
+  char getFMSColour = getFMSColour();
+ 
   public char getFMSColour() {
     String gameData;
     gameData = DriverStation.getInstance().getGameSpecificMessage();
     if (gameData.length() > 0) {
-      return gameData.charAt(0);
+      colourGoal = gameData.charAt(0);
+      return colourGoal;
     } else {
-      return ' ';
+      return 'n';
     }
-    //   switch (gameData.charAt(0))
-  //   {
-  //     case 'B' :
-  //       //Blue case code
-  //       break;
-  //     case 'G' :
-  //       //Green case code
-  //       break;
-  //     case 'R' :
-  //       //Red case code
-  //       break;
-  //     case 'Y' :
-  //       //Yellow case code
-  //       break;
-  //     default :
-  //       //This is corrupt data
-  //       break;
-  //   }
-  // } else {
-  //   //Code for no data received yet
-  // }
-
   }
-
-  public String getColour(){
-    return "a";
-  }
-
-  public int colourChange(){
-    return 3;
-  }
-
-
  
-  
+  char getColourFromSensor;
+ 
+  private char getColourFromSensor() {
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);  
+    char result;
+    Color detectedColor = m_colorSensor.getColor();
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+    if (match.color == kBlueTarget) {
+      result = 'b';
+    } else if (match.color == kRedTarget) {
+      result = 'r';
+    } else if (match.color == kGreenTarget) {
+      result = 'g';
+    } else if (match.color == kYellowTarget) {
+      result = 'y';
+    } else {
+      result = 'n';
+    }
+    return result;
+  }
+ 
+ 
  
  
 }
- 
  
 
