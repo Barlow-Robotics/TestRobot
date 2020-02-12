c79ede5a7ed1bc9637d01193f4b0d5e5d99964c9
ReillyO <57842556+ReillyO@users.noreply.github.com>
2020-02-12 15:40:02 -0500

    Update OI.java

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -12,55 +12,23 @@ public class OI{
         controller = new Joystick(Constants.joystickPort);
     }
 
-    public double getLeftStick(){
-        return controller.getRawAxis(1);
-    }
+    public double getLeftY(){return controller.getRawAxis(1);}
+    public double getRightX(){return controller.getRawAxis(2);}
 
-    public double getRightStick(){
-        return controller.getRawAxis(3);
-    }
+    public boolean getSquareButton(){return controller.getRawButton(1);}
+    public boolean getCircleButton(){return controller.getRawButton(3);}
+    public boolean getXButton(){return controller.getRawButton(2);}
 
-    public boolean getSquareButton(){
-        return controller.getRawButton(1);
-    }
+    public boolean getLTopTrigger() {return controller.getRawButton(5);}
+    public boolean getLBottomTrigger() {return controller.getRawButton(7);}
 
-    public boolean getCircleButton(){
-        return controller.getRawButton(3);
-    }
+    public boolean getRTopTrigger() {return controller.getRawButton(6);}
+    public boolean getRBottomTrigger() {return controller.getRawButton(8);}
 
-    public boolean getXButton(){
-        return controller.getRawButton(2);
-    }
-
-    public boolean getLTopTrigger() {
-        return controller.getRawButton(5);
-    }
-
-    public boolean getLBottomTrigger() {
-        return controller.getRawButton(7);
-    }
-
-    public boolean getRTopTrigger() {
-        return controller.getRawButton(6);
-        //climb - up
-    }
-
-    public boolean getRBottomTrigger() {
-        return controller.getRawButton(8);
-        //climb - move down
-    }
-
-    public boolean getPOVUp(){
-        return controller.getPOV() == 1;
-    }
-
-    public boolean getPOVDown(){
-        return controller.getPOV() == 5;
-    }
-
-    public boolean getPOVLeft(){
-        return controller.getPOV() == 3;
-    }
+    public boolean getPOVUp(){return controller.getPOV() == 1;}
+    public boolean getPOVDown(){return controller.getPOV() == 5;}
+    public boolean getPOVLeft(){return controller.getPOV() == 3;}
+    public boolean getPOVRight(){return controller.getPOV() == 7;}
 
     public void publishData(double data){
         SmartDashboard.putNumber("Deploy Speed", data);
