70def11aeac57286aac95113c01d6df14cb6a0d5
ReillyO <57842556+ReillyO@users.noreply.github.com>
2020-02-14 13:29:38 -0500

    Update ColourFilter.java

--- src/main/java/frc/robot/components/ColourFilter.java
+++ src/main/java/frc/robot/components/ColourFilter.java
@@ -10,27 +10,24 @@ package frc.robot.components;
  
 import java.util.ArrayList;
 import java.util.List;
-import frc.robot.subsystems.ArmSubsystem;
  
 public class ColourFilter {
     char currentColour;
     int filterLength;
     List<Character> listOfColours;
  
-    ArmSubsystem armSubsystem = new ArmSubsystem();
- 
-public void addMeasurement(char c){
-    listOfColours.add(c);
-    if (listOfColours.size()>filterLength){
-        listOfColours.remove(0);
+    public void addMeasurement(char c){
+        listOfColours.add(c);
+        if (listOfColours.size()>filterLength){
+            listOfColours.remove(0);
+        }
+        for (char x: listOfColours){
+            if (x!=c) {
+                return;
+            }  
+        }  
+        currentColour = c;
     }
-    for (char x: listOfColours){
-        if (x!=c) {
-            return;
-        }  // if
-    }  // for
-    currentColour = c;
-}
 
 
  
@@ -41,8 +38,8 @@ public char getColour(){
 public ColourFilter(int length, char startingColour){
     listOfColours = new ArrayList<Character>();
     currentColour = startingColour;
-    filterLength=length;
-    }
+    filterLength = length;
+}
  
 }
 
