5cb04ac0749f368dd00c5a9ab9c2718b0bc4db28
ReillyO <57842556+ReillyO@users.noreply.github.com>
2020-02-14 13:29:55 -0500

    Create IntakeSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import frc.robot.OI;
+
+/**
+ * Add your docs here.
+ */
+
+
+
+public class IntakeSubsystem extends Subsystem {
+  private Solenoid intakeDeploy, intakeRetract;
+  private OI oi;
+
+  private enum IntakeState{
+    RetractedIdle,
+    Deploying,
+    Intaking,
+    Retracting
+  }
+  
+  IntakeState intakeState;
+
+  public IntakeSubsystem(){
+    oi = new OI();
+//    u
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void operateIntake(){
+
+    switch(intakeState){
+
+    }
+  }
+}
