38ca38649586145a9eccd2158f2ae879773466e9
ReillyO <57842556+ReillyO@users.noreply.github.com>
2020-02-15 10:08:30 -0500

    Update ArmSubsystem.java

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -58,7 +58,7 @@ public class ArmSubsystem extends Subsystem {
   private Solenoid closeSolenoidDeploy = new Solenoid(Constants.closeSolenoidDeployPort);
   private Compressor compressor = new Compressor(0);
   private boolean wheelDeployed, colorMatched;
-  private WPI_TalonSRX wheelSpinner = new WPI_TalonSRX(Constants.wheelRotationTalonID);
+  private WPI_TalonSRX wheelSpinner = new WPI_TalonSRX(1);
  
   ColourFilter colourFilter;
  
@@ -83,7 +83,6 @@ public class ArmSubsystem extends Subsystem {
     previousInput = false;
     wheelDeployed = false;
     spinningMode = SpinningMode.Which;
-    compressor.setClosedLoopControl(true);
   }
  
   @Override
@@ -143,6 +142,7 @@ public class ArmSubsystem extends Subsystem {
             colourFilter.addMeasurement(getColourFromSensor());
             currentColour = colourFilter.getColour();
             if (currentColour != lastColour) {
+              System.out.println("Change:"+colourChangeCounter);
               colourChangeCounter++;
               lastColour = currentColour;
               System.out.println("Color changed ("+colourChangeCounter+")");
