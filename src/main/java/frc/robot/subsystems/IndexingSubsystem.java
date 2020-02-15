f0d79c0783fe59b73bfe9b7379c927e37bd1ffad
ReillyO <57842556+ReillyO@users.noreply.github.com>
2020-02-15 13:00:33 -0500

    Update IndexingSubsystem.java

--- src/main/java/frc/robot/subsystems/IndexingSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexingSubsystem.java
@@ -26,8 +26,8 @@ public class IndexingSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   //private WPI_TalonSRX wheelDriver;
-  private Spark indexingWheelDriver;
-  private Spark agitatorMotor;
+  private WPI_TalonSRX indexingWheelDriver;
+  private WPI_TalonSRX agitatorMotor;
   private OI oi;
   private int cellCount;
   enum IndexingState{
@@ -41,7 +41,8 @@ public class IndexingSubsystem extends Subsystem {
 
   public IndexingSubsystem(){
 //    wheelDriver = new WPI_TalonSRX(Constants.indexingWheelMotor);
-    indexingWheelDriver = new Spark(Constants.indexingWheelMotor);
+    indexingWheelDriver = new WPI_TalonSRX(7);
+    agitatorMotor = new WPI_TalonSRX(6);
     oi = new OI();
     cellCount = 0;
     indexingState = IndexingState.Idle;
@@ -53,32 +54,32 @@ public class IndexingSubsystem extends Subsystem {
     // setDefaultCommand(new MySpecialCommand());
   }
 
-  public void operateIndex(ShooterState shooterState, IntakeState intakeState){
+  public void operateIndex(){
     
     switch(indexingState){
       case Idle:
-        if(shooterState == ShooterState.Targeting)
+        if(oi.getIsShooting())
           indexingState = IndexingState.Feeding;
-        else if(intakeState == IntakeState.Intaking)
+        else if(oi.isBallChasing())
           indexingState = IndexingState.Agitating;
         else
           indexingWheelDriver.set(0.0);
         break;
       case Feeding:
-        if(shooterState == ShooterState.IdleSpin){
+        if(!oi.getIsShooting()){
           indexingWheelDriver.set(0);
           agitatorMotor.set(0);
           indexingState = IndexingState.Idle;
         }
         else{
-          indexingWheelDriver.set(Constants.feedingSpeed);
+          indexingWheelDriver.set(-Constants.feedingSpeed);
           // agitatorMotor.set(Constants.agitatingSpeed);
           if(false/*Sensor detects ball exit*/)
             cellCount--;
         }
         break;
       case Agitating:
-        if(intakeState == IntakeState.RetractedIdle){
+        if(!oi.isBallChasing()){
           agitatorMotor.set(0);
           indexingState = IndexingState.Idle;
         }
