a2604b89ee3737f36bc2ae68e57ab264c5b481a1
ReillyO <57842556+ReillyO@users.noreply.github.com>
2020-02-21 18:49:43 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -141,6 +141,7 @@ public class Robot extends TimedRobot {
     m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
+    autoState = AutoState.DrivingPath;
   }
 
   /**
