ea0da5dae92953e4d4703bae51d0040a4f1df73e
ReillyO <57842556+ReillyO@users.noreply.github.com>
2020-02-22 12:02:40 -0500

    Update PathParams.java

--- src/main/java/frc/robot/components/PathParams.java
+++ src/main/java/frc/robot/components/PathParams.java
@@ -1,13 +1,21 @@
 package frc.robot.components;
 
-
+import frc.robot.Constants;
 
 public class PathParams {
+    private double leftRotations, rightRotations;
+    private double radius, degreeChangeInRadians;
 
 
-    public PathParams(){
-
+    public PathParams(double degreeChangeInRadians, double radius){
+        this.degreeChangeInRadians = degreeChangeInRadians;
+        this.radius = radius;
+        leftRotations = (radius + Constants.halfDistanceBetweenWheels) * degreeChangeInRadians/(2 * Math.PI);
+        rightRotations = (radius - Constants.halfDistanceBetweenWheels) * degreeChangeInRadians/(2 * Math.PI);
     }
 
+    public double getLeftRotations(){
+        return leftRotations;
+    }
     
 }
