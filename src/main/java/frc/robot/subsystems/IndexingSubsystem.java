17ae7c4d601007a3b1d1c56f7dc8f1dad4b67172
ReillyO <57842556+ReillyO@users.noreply.github.com>
2020-03-12 10:25:52 -0400

    Update IndexingSubsystem.java

--- src/main/java/frc/robot/subsystems/IndexingSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexingSubsystem.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
@@ -30,6 +31,7 @@ public class IndexingSubsystem extends Subsystem {
   // here. Call these from Commands.
   //private WPI_TalonSRX wheelDriver;
   private WPI_TalonSRX indexingWheelDriver;
+  private WPI_TalonSRX shooterController;
   private Spark agitatorMotor;
   private int cellCount;
   private BeamSensor exitSensor;
@@ -38,10 +40,24 @@ public class IndexingSubsystem extends Subsystem {
   private boolean agitating;
   private double agitatingCycleStartTime;
   private double cycleStart;
+  private double firingStartTime;
+
+
+  private double flywheelSpeedPercent = 0;
+  private double servoAngle = 0;
+  private NetworkTableEntry distanceToTarget;
+  private NetworkTableEntry kF_Shooter;
+  private NetworkTableEntry kP_Shooter;
+  private NetworkTableEntry kI_Shooter;
+  private NetworkTableEntry kD_Shooter;
+
+
+
   enum IndexingState{
     Idle,
-    Feeding,
-    ProcessingIntake,
+    SpinningUp,
+    Firing,
+    Agitating,
     ManualIntake
   }
 
@@ -60,11 +76,15 @@ public class IndexingSubsystem extends Subsystem {
   private NetworkTableEntry kI_Index;
   private NetworkTableEntry kD_Index;
   private NetworkTableEntry cellCountEntry;
+  private NetworkTableEntry targetDistanceEntry ;
+
+
 
   public IndexingSubsystem(NetworkTableInstance networkTableInst, int cellCount){
     indexingWheelDriver = new WPI_TalonSRX(Constants.ID_shooterFeedMotor);
+    shooterController = new WPI_TalonSRX(Constants.ID_shooterMotor);
     agitatorMotor = new Spark(Constants.PWMPORT_agitatorMotor);
-    cellCount = 0;
+    this.cellCount = cellCount;
     indexingState = IndexingState.Idle;
 
     this.networkTableInst = networkTableInst;
@@ -87,12 +107,41 @@ public class IndexingSubsystem extends Subsystem {
     indexingWheelDriver.config_kI(0, (double)kI_Index.getNumber(0.000));
     indexingWheelDriver.config_kD(0, (double)kD_Index.getNumber(Constants.indexingkD));
 
+    shooterController.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, Constants.mainFeedbackLoop, Constants.timeoutTime);
+    shooterController.configNominalOutputForward(0.0);
+    shooterController.configNominalOutputReverse(0.0);
+    shooterController.configPeakOutputForward(1.0);
+    shooterController.configPeakOutputReverse(-1.0);
+
+    this.networkTableInst = networkTableInst;
+    distanceToTarget = networkTableInst.getTable("shooter").getEntry("distanceToTarget");
+
+    kF_Shooter = networkTableInst.getTable("shooter").getEntry("kF_Shooter");
+    kP_Shooter = networkTableInst.getTable("shooter").getEntry("kP_Shooter");
+    kI_Shooter = networkTableInst.getTable("shooter").getEntry("kI_Shooter");
+    kD_Shooter = networkTableInst.getTable("shooter").getEntry("kD_Shooter");
+
+    kF_Shooter.setNumber(Constants.shooterkF);
+    kP_Shooter.setNumber(Constants.shooterkP);
+    kI_Shooter.setNumber(0.000);
+    kD_Shooter.setNumber(Constants.shooterkD);
+
+    targetDistanceEntry = networkTableInst.getTable("vision").getEntry("targetDistance") ;
+
     exitSensor = new BeamSensor(Constants.DIOPORT_exitSensor, Constants.DIOPORT_exitTransmitter);
     entrySensor = new BeamSensor(Constants.DIOPORT_intakeSensor, Constants.DIOPORT_intakeTransmitter);
     
+    shooterController.configMotionCruiseVelocity(8192 * 10000);
+    shooterController.config_kF(0, (double)kF_Shooter.getNumber(Constants.shooterkF)); 
+    shooterController.config_kP(0, (double)kP_Shooter.getNumber(Constants.shooterkP));
+    shooterController.config_kI(0, (double)kI_Shooter.getNumber(0.000));
+    shooterController.config_kD(0, (double)kD_Shooter.getNumber(Constants.shooterkD));
+
     agitating = true;
     agitatingCycleStartTime = 0;
     cycleStart = 0;
+
+    firingStartTime = 0.0;
   } 
 
 
@@ -105,69 +154,97 @@ public class IndexingSubsystem extends Subsystem {
 
 
   
-  public void operateIndex(boolean firing, boolean manualIntake, boolean cellChasing){
+  //OO COMMENTED OUT ALL AGITATOR MOTOR REFERENCES EXCEPT FOR TOP
+  public void operateIndex(boolean firing, boolean manualIntake, boolean cellChasing, boolean agitate){
+    double agitatorSpeed = Constants.agitatingSpeed;
+    if(!agitate)
+      agitatorSpeed = 0.0;
+    else
+      agitatorSpeed = Constants.agitatingSpeed;
+    agitatorMotor.set(agitatorSpeed);
+    // wpk need to clean up all these magic numbers
+    double shooterSpeedScale = Constants.desiredShooterPercent ;
+    double distanceToTarget = targetDistanceEntry.getDouble( -1.0) ;
+    if ( distanceToTarget > 0.0) {
+      //OO FIX SHOOTER PERCENT HERE
+      shooterSpeedScale = Constants.desiredShooterPercent +  0.10 * ((( distanceToTarget / 1000) * 3.281) -10.0 ) / ( 31.0 -10.0) ;
+    }
     keepTransmittersActive();
     updatePIDValues();
     switch(indexingState){
       case Idle:
         if(firing){
-          indexingState = IndexingState.Feeding;
+          indexingState = IndexingState.SpinningUp;
           agitatingState = AgitatingState.Clockwise;
           agitatingCycleStartTime = System.currentTimeMillis();
+//        shooterController.set(ControlMode.Velocity, Constants.desiredShooterPercent * Constants.maxShooterSpeed);
+          shooterController.set(ControlMode.Velocity, shooterSpeedScale * Constants.maxShooterSpeed);
+          firingStartTime = System.currentTimeMillis();
         }
         else if(cellChasing)
-          indexingState = IndexingState.ProcessingIntake;
+          indexingState = IndexingState.Agitating;
         else if(manualIntake)
           indexingState = IndexingState.ManualIntake;
         else {
           indexingWheelDriver.set(0.0);
-          agitatorMotor.set(0.0);
+          // agitatorMotor.set(0.0);
+          shooterController.set(0.0);
         }
         break;
-      case Feeding:
+      case SpinningUp:
+        if(shooterController.getSelectedSensorVelocity() >= Constants.minShooterPercentForFiring * shooterController.getClosedLoopTarget()
+           || System.currentTimeMillis() - firingStartTime >= Constants.shooterSpinupTimeout){
+          indexingState = IndexingState.Firing;
+        }
+        else if(!firing){
+          shooterController.set(0.0);
+          indexingState = IndexingState.Idle;
+        }
+        // shooterController.set(ControlMode.Velocity, Constants.desiredShooterPercent * Constants.maxShooterSpeed);
+        shooterController.set(ControlMode.Velocity, shooterSpeedScale * Constants.maxShooterSpeed);
+        break;
+      case Firing:
         if(!firing){
           indexingWheelDriver.set(0);
-          agitatorMotor.set(0);
+          shooterController.set(0.0);
+          // agitatorMotor.set(0);
           indexingState = IndexingState.Idle;
           agitatingState = AgitatingState.Off;
         }
         else{
           indexingWheelDriver.set(ControlMode.Velocity, Constants.desiredFeedingPercent * Constants.maxFeedingVelocity);
+//          shooterController.set(ControlMode.Velocity, Constants.desiredShooterPercent * Constants.maxShooterSpeed);
+          shooterController.set(ControlMode.Velocity, shooterSpeedScale * Constants.maxShooterSpeed);
           // if(agitating)
-            agitatorMotor.set(Constants.agitatingSpeed);
+          //   agitatorMotor.set(agitatorSpeed);
           // else
             // agitatorMotor.set(0.0);
 
-          if(System.currentTimeMillis() - agitatingCycleStartTime >= Constants.agitatorCyclePeriod){
-            agitating = !agitating;
-            agitatingCycleStartTime = System.currentTimeMillis();
-          }
-          SmartDashboard.putNumber("Agitation", agitatorMotor.get());
           if(ballHasExited())
             cellCount--;
         }
         updatePreviousValues();
         break;
-      case ProcessingIntake:
+      case Agitating:
         if(!cellChasing){
-          agitatorMotor.set(0);
+          // agitatorMotor.set(0);
           indexingState = IndexingState.Idle;
         }
         else{
           if(ballHasEntered())
             cellCount++;
-          agitatorMotor.set(Constants.agitatingSpeed);
+          // agitatorMotor.set(agitatorSpeed);
         }
       break;
       case ManualIntake:
         if(!manualIntake){
-          agitatorMotor.set(0);
+          // agitatorMotor.set(0);
           indexingState = IndexingState.Idle;
         }
         else{
           if(ballHasEntered())
             cellCount++;
-          agitatorMotor.set(Constants.agitatingSpeed);
+          // agitatorMotor.set(agitatorSpeed);
         }
       break;
     } 
